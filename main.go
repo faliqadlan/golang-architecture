699282ef85e60b8d3eef57a6879f83bdd725a3f9
Faliq Adlan <m.faliq.adlan@roketin.com>
2022-09-14 09:33:21 +0700

    test:

--- main.go
+++ main.go
@@ -1,11 +1,54 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
+import "fmt"
+
+type person struct {
+	first string
+}
+
+func (p person) speak() {
+	fmt.Println("from a person - this is my name", p.first)
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (sa secretAgent) speak() {
+	fmt.Println("I'm a secret agent - this is my name", sa.first)
+}
+
+// any TYPE that has the methods specified by an interface
+// is also of the interface type
+// an interface says
+// "Hey baby, if you have these methods, then you're my type."
+
+type human interface {
+	speak()
+}
 
 func main() {
-	fmt.Println("Hello, world!")
-	log.Print("Hello, world!")
+	p1 := person{
+		first: "Miss Moneypenny",
+	}
+
+	sa1 := secretAgent{
+		person: person{
+			first: "James",
+		},
+		ltk: true,
+	}
+
+	fmt.Printf("%T\n", p1)
+
+	// in go a VALUE can be of more than one TYPE
+	// in this example, p1 is both TYPE person and TYPE human
+	var x, y human
+	x = p1
+	y = sa1
+	fmt.Println(x,y)
+	p1.speak()
+	sa1.speak()
+	x.speak()
 }
